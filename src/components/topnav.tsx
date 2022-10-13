11434bcc27ffc9ca4f3e4e67176dc103914f4fcb
SizzlingHot <jimmyzakis@gmail.com>
2022-10-12 20:00:01 -0400

    Base Website Setup

---
+++ src/components/topnav.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function topnav() {
+    return (
+        <>
+            <Navbar bg="dark">
+                <Container>
+                    <Navbar.Brand>Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>Features</Nav.Link>
+                        <Nav.Link>Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+
+export default topnav;
